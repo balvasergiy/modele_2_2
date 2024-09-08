81e39da07f13ba6c25d4d77adcffe97892687f7e
balvasergiy <balvasergiy@yandex.ru>
2024-09-08 22:14:02 +0400

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+first=123
+second=456
+third=789
+if first==second and first==third and second==third:
+    print(3)
+a=int(42)
+b=int(69)
+c=int(42)
+if a==b or a==c or b==c:
+    print(2)
+x=123
+y=456
+z=789
+if not x == y and not y == z and not x == z:
+    print(0)
